026b8c9219fa02aaa65c3c345a8993e56a0e6c4c
chidiokoroafor <cokoroafor@healthstation.ng>
2025-01-02 03:21:38 +0100

    fix image

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -21,12 +21,12 @@ const Details = () => {
       
       {isLoading ? <p className="text-center text-2xl mt-12 dark:text-White">Loading...</p> :
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
-          <div className="border">
-            <img src={country?.flags?.svg} alt="" />
+          <div className=" w-full">
+            <img className="w-full object-cover" src={country?.flags?.svg} alt="" />
           </div>
-          <div className="borde px-8 dark:text-White">
-            <h2 className="font-[800] text-3xl mb-6">{country?.name.common}</h2>
-            <div className="flex justify-between ">
+          <div className="borde px-1 dark:text-White">
+            <h2 className="font-[800] text-3xl mb-8">{country?.name.common}</h2>
+            <div className="grid grid-cols-2 ">
               <div className="space-y-2">
                 <p><strong>Native Name:</strong> {country?.name.common}</p>
                 <p><strong>Population:</strong> {country?.population}</p>
@@ -34,16 +34,16 @@ const Details = () => {
                 <p><strong>Sub Region:</strong> {country?.subregion}</p>
                 <p><strong>Capital:</strong> {country?.capital + ""}</p>
               </div>
-              <div className="space-y-2">
+              <div className="space-y-2 ">
                 <p><strong>Top Level Domain: </strong>{country?.tld && country?.tld[0]}</p>
                       
                 <p><strong>Currencies: </strong>{country?.currencies && Object.keys(country?.currencies)[0]} </p>
                       
-                <p><strong>Languages: </strong> {country?.languages && Object.values(country?.languages) + ""}</p>
+                <p><strong>Languages: </strong> {country?.languages && Object.values(country?.languages).join(", ")}</p>
                     
               </div>
             </div>
-            <div className="flex gap-2 mt-10">
+            <div className="flex gap-2 mt-16">
               <strong>Border Countries: </strong>
               <div className="flex gap-2">
                 {country?.borders && country?.borders.map((bord) => {
